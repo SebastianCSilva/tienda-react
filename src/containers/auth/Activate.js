ee30aacaddb7ad91e466912da1d60649d669265a
SebastianCSilva <sebastian.cornejo.silva@gmail.com>
2023-05-04 21:55:10 -0400

    Se agregan las bases para la activacion de cuentas

--- src/containers/auth/Activate.js
+++ src/containers/auth/Activate.js
@@ -1,5 +1,11 @@
 import Layout from '../../hocs/Layout'
+import { useParams } from 'react-router'
 const Activate = () =>{
+    const params = useParams()
+
+    const uid = params.uid
+    const token = params.token
+
     return(
         <Layout>Activate</Layout>
     )
