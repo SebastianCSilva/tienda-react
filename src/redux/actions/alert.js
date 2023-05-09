e9487d9a177ddb3649b010ea71be7dfffa5fd980
SebastianCSilva <sebastian.cornejo.silva@gmail.com>
2023-05-08 23:54:20 -0400

    Se agregan acciones para alertas

---
+++ src/redux/actions/alert.js
@@ -0,0 +1,15 @@
+import { timeout } from 'workbox-core/_private';
+import {
+    SET_ALERT,
+    REMOVE_ALERT
+} from './types';
+
+export const setAlert = (msg, alertType, timeout = 5000) => dispatch => {
+    dispatch({
+        type: SET_ALERT,
+        payload: { msg, alertType }
+    });
+
+    setTimeout(() => dispatch({ type: REMOVE_ALERT }), timeout);
+    
+}
