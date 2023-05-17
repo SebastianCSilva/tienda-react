c1e81bb962301f829b67ea52536272e96f5f044f
SebastianCSilva <sebastian.cornejo.silva@gmail.com>
2023-05-17 00:06:51 -0400

    Se agrega menu interactivo en el navbar para usuarios invitados y logueados

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -1,8 +1,9 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Bars3Icon, BellIcon, XMarkIcon, ChevronDownIcon } from '@heroicons/react/24/outline'
 import { Link } from 'react-router-dom'
 import Alert from '../../components/alert'
+import { connect } from 'react-redux'
 
 const navigation = [
   { name: 'Tienda', href: '#', current: true },
@@ -15,7 +16,75 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
-export default function Navbar() {
+function Navbar({
+  isAuthenticated,
+  user
+}) {
+
+  const authLinks = (
+    <Menu as="div" className="relative ml-3">
+      <div>
+        <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+        <span className="inline-block h-10 w-10 rounded-full overflow-hidden bg-gray-100">
+          <svg className="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
+            <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+          </svg>
+        </span>
+        </Menu.Button>
+      </div>
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <Menu.Item>
+            {({ active }) => (
+              <Link
+                to="/login"
+                className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+              >
+                Sign in
+              </Link>
+            )}
+          </Menu.Item>
+          <Menu.Item>
+            {({ active }) => (
+              <Link
+                to="/signup"
+                className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+              >
+                Sign up
+              </Link>
+            )}
+          </Menu.Item>
+          
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  )
+
+  const guestLinks = (
+    <Fragment>
+      <Link
+        to="/login"
+        className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+      >
+        Iniciar sesion
+      </Link>
+      <Link
+        to="/signup"
+        className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+      >
+        Registrar
+      </Link>
+    </Fragment>
+  )
+
   return (
     <>
     <Disclosure as="nav" className="bg-gray-800">
@@ -69,50 +138,11 @@ export default function Navbar() {
                 
 
                 {/* Profile dropdown */}
-                <Menu as="div" className="relative ml-3">
-                  <div>
-                    <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                      <span className="sr-only">Open user menu</span>
-                      
-                      <div class="relative w-10 h-10 overflow-hidden bg-inherit rounded-full ">
-                        <svg class="absolute w-12 h-12 text-gray-300 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
-                      </div>
-                    </Menu.Button>
-                  </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <Link
-                            to="/login"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Sign in
-                          </Link>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <Link
-                            to="/signup"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Sign up
-                          </Link>
-                        )}
-                      </Menu.Item>
-                      
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
+                {
+                  isAuthenticated ? authLinks:guestLinks
+                }
+
+
               </div>
             </div>
           </div>
@@ -142,3 +172,12 @@ export default function Navbar() {
     </>
   )
 }
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.Auth.isAuthenticated,
+  user: state.Auth.user
+})
+
+export default connect(mapStateToProps, {
+
+}) (Navbar)
