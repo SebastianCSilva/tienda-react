1b15b709bd67fa75aab7007ae9c4514d712ca16a
SebastianCSilva <sebastian.cornejo.silva@gmail.com>
2023-05-25 23:22:34 -0400

    Se agrega la accion para confirmar el reset de password

--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -322,6 +322,64 @@ export const reset_password = (email) => async dispatch => {
 
 }
 
+export const reset_password_confirm = (uid, token, new_password, re_new_password) => async dispatch => {
+    dispatch({
+        type: SET_AUTH_LOADING
+    });
+
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    const body = JSON.stringify({
+        uid,
+        token,
+        new_password,
+        re_new_password
+    });
+    if (new_password !== re_new_password) {
+        dispatch({
+            type: RESET_PASSWORD_CONFIRM_FAIL
+        });
+        dispatch({
+            type: REMOVE_AUTH_LOADING
+        });
+        dispatch(setAlert('Password do not match', 'red'));
+    } else {
+        try {
+            const res = await axios.post(`${process.env.REACT_APP_API_URL}/auth/users/reset_password_confirm/`, body, config);
+
+            if (res.status === 204){
+                dispatch({
+                    type: RESET_PASSWORD_CONFIRM_SUCCESS
+                });
+                dispatch({
+                    type: REMOVE_AUTH_LOADING
+                });
+                dispatch(setAlert('Password has been reset successfully', 'green'));
+            } else {
+                dispatch({
+                    type: RESET_PASSWORD_CONFIRM_FAIL
+                });
+                dispatch({
+                    type: REMOVE_AUTH_LOADING
+                });
+                dispatch(setAlert('Error resetting your password', 'red'));
+            }
+        } catch(err) {
+            dispatch({
+                type: RESET_PASSWORD_CONFIRM_FAIL
+            });
+            dispatch({
+                type: REMOVE_AUTH_LOADING
+            });
+            dispatch(setAlert('Error resetting your password', 'red'));
+        }
+    }
+
+}
 
 export const logout = () => dispatch => {
     dispatch({
