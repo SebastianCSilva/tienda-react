e053c8d68da6e38c8e7c15ca7b36a1ce9930d444
SebastianCSilva <sebastian.cornejo.silva@gmail.com>
2023-06-15 22:27:14 -0400

    Se corrigen problemas de iconos con heroicons 1 y 2

--- src/containers/Shop.jsx
+++ src/containers/Shop.jsx
@@ -1,8 +1,9 @@
 import Layout from '../hocs/Layout'
 import { Fragment, useState, useEffect } from 'react'
+
 import { Dialog, Disclosure, Menu, Transition } from '@headlessui/react'
-import { XIcon } from '@heroicons/react/outline'
-import { ChevronDownIcon, FilterIcon, MinusSmIcon, PlusSmIcon, ViewGridIcon } from '@heroicons/react/solid'
+import { MagnifyingGlassIcon, ChevronDownIcon, FunnelIcon, PlusIcon, MinusSmallIcon } from '@heroicons/react/20/solid'
+
 
 import { connect } from 'react-redux'
 import { get_categories } from '../redux/actions/categories'
@@ -109,7 +110,7 @@ const Shop = ({
                                 onClick={() => setMobileFiltersOpen(false)}
                             >
                                 <span className="sr-only">Close menu</span>
-                                <XIcon className="h-6 w-6" aria-hidden="true" />
+                                <MagnifyingGlassIcon className="h-6 w-6" aria-hidden="true" />
                             </button>
                             </div>
 
@@ -135,9 +136,9 @@ const Shop = ({
                                         <span className="font-medium text-gray-900">{section.name}</span>
                                         <span className="ml-6 flex items-center">
                                             {open ? (
-                                            <MinusSmIcon className="h-5 w-5" aria-hidden="true" />
+                                            <MinusSmallIcon className="h-5 w-5" aria-hidden="true" />
                                             ) : (
-                                            <PlusSmIcon className="h-5 w-5" aria-hidden="true" />
+                                            <PlusIcon className="h-5 w-5" aria-hidden="true" />
                                             )}
                                         </span>
                                         </Disclosure.Button>
@@ -224,7 +225,7 @@ const Shop = ({
 
                         <button type="button" className="p-2 -m-2 ml-5 sm:ml-7 text-gray-400 hover:text-gray-500">
                             <span className="sr-only">View grid</span>
-                            <ViewGridIcon className="w-5 h-5" aria-hidden="true" />
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"> <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM14 11a1 1 0 011 1v1h1a1 1 0 110 2h-1v1a1 1 0 11-2 0v-1h-1a1 1 0 110-2h1v-1a1 1 0 011-1z" /> </svg>
                         </button>
                         <button
                             type="button"
@@ -232,7 +233,7 @@ const Shop = ({
                             onClick={() => setMobileFiltersOpen(true)}
                         >
                             <span className="sr-only">Filters</span>
-                            <FilterIcon className="w-5 h-5" aria-hidden="true" />
+                            <FunnelIcon className="w-5 h-5" aria-hidden="true" />
                         </button>
                         </div>
                     </div>
@@ -263,9 +264,9 @@ const Shop = ({
                                         <span className="font-medium text-gray-900">{section.name}</span>
                                         <span className="ml-6 flex items-center">
                                         {open ? (
-                                            <MinusSmIcon className="h-5 w-5" aria-hidden="true" />
+                                            <MinusSmallIcon className="h-5 w-5" aria-hidden="true" />
                                         ) : (
-                                            <PlusSmIcon className="h-5 w-5" aria-hidden="true" />
+                                            <PlusIcon className="h-5 w-5" aria-hidden="true" />
                                         )}
                                         </span>
                                     </Disclosure.Button>
