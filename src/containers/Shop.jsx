1ee877fc9eb3e27ad12f0cd89d00e3ffdd255796
SebastianCSilva <sebastian.cornejo.silva@gmail.com>
2023-06-19 21:21:07 -0400

    Se agrega los filtros de categorias para desktop

--- src/containers/Shop.jsx
+++ src/containers/Shop.jsx
@@ -291,11 +291,55 @@ const Shop = ({
                         <form className="hidden lg:block">
                             <h3 className="sr-only">Categories</h3>
                             <ul role="list" className="text-sm font-medium text-gray-900 space-y-4 pb-6 border-b border-gray-200">
-                            {subCategories.map((category) => (
-                                <li key={category.name}>
-                                <a href={category.href}>{category.name}</a>
-                                </li>
-                            ))}
+                            {
+                                    categories &&
+                                    categories !== null &&
+                                    categories !== undefined &&
+                                    categories.map(category => {
+                                        if (category.sub_categories.length === 0){
+                                            return(
+                                                <div key={category.id} className='flex items-center h-5 my-5'>
+                                                    <input name='category_id'
+                                                        type='radio'
+                                                        className='focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300 rounded-full'
+                                                    />
+                                                    <label className='ml-3 min-w-0 flex-1 text-gray-500'>
+                                                        {category.name}
+                                                    </label>
+                                                </div>
+                                            )
+                                        } else {
+                                            let result = []
+                                            result.push(
+                                                <div key={category.id} className='flex items-center h-5'>
+                                                    <input name='category_id'
+                                                        type='radio'
+                                                        className='focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300 rounded-full'
+                                                    />
+                                                    <label className='ml-3 min-w-0 flex-1 text-gray-500'>
+                                                        {category.name}
+                                                    </label>
+                                                </div>
+                                            )
+
+                                            category.sub_categories.map(sub_category => {
+                                                result.push(
+                                                    <div key={sub_category.id} className='flex items-center h-5 ml-2 my-5'>
+                                                        <input name='category_id'
+                                                            type='radio'
+                                                            className='focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300 rounded-full'
+                                                        />
+                                                        <label className='ml-3 min-w-0 flex-1 text-gray-500'>
+                                                            {sub_category.name}
+                                                        </label>
+                                                    </div>
+                                                )
+                                            })
+
+                                            return result
+                                        }
+                                    })
+                                }
                             </ul>
 
                             {filters.map((section) => (
