4b67ac28d1bd8596ae1d5ea3d6ee7db4398fb215
SebastianCSilva <sebastian.cornejo.silva@gmail.com>
2023-07-05 22:32:05 -0400

    Se agrega la accion para obtener los productos por vendidos

--- src/redux/actions/products.js
+++ src/redux/actions/products.js
@@ -71,3 +71,31 @@ export const get_products_by_arrival = () => async dispatch => {
         });
     }
 }
+
+export const get_products_by_sold = () => async dispatch => {
+    const config = {
+        headers: {
+            'Accept': 'application/json'
+        }
+    };
+
+    try {
+        const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/product/get-products?sortBy=sold&order=desc&limit=3`, config);
+
+        if (res.status === 200) {
+            dispatch({
+                type: GET_PRODUCTS_BY_SOLD_SUCCESS,
+                payload: res.data
+            });
+        } else {
+            dispatch({
+                type: GET_PRODUCTS_BY_SOLD_FAIL
+            });
+        }
+
+    }catch(err){
+        dispatch({
+            type: GET_PRODUCTS_BY_SOLD_FAIL
+        });
+    }
+}
