4dcebe38cc61bd9ec7e1c4d504770851d69aa851
SebastianCSilva <sebastian.cornejo.silva@gmail.com>
2023-07-13 23:59:40 -0400

    Se traen los productos al home

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,5 +1,25 @@
 import Layout from "../hocs/Layout"
-const Home = () => {
+import { connect } from "react-redux";
+import {
+    get_products_by_arrival,
+    get_products_by_sold
+} from '../redux/actions/products';
+import { useEffect } from "react";
+
+const Home = ({
+    get_products_by_arrival,
+    get_products_by_sold,
+    products_arrival,
+    products_sold
+}) => {
+
+    useEffect(() => {
+        window.scrollTo(0,0);
+
+        get_products_by_arrival();
+        get_products_by_sold();
+    }, []);
+
     return(
         <Layout>
             Home
@@ -7,4 +27,12 @@ const Home = () => {
     )
 }
 
-export default Home
+const mapStateToProps = state => ({
+    products_arrival: state.Products.products_arrival,
+    products_sold: state.Products.products_sold,
+})
+
+export default connect(mapStateToProps,{
+    get_products_by_arrival,
+    get_products_by_sold
+}) (Home)
