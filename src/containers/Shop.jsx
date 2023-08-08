633e010a37d63dba7919eae82345286c8ed37a80
SebastianCSilva <sebastian.cornejo.silva@gmail.com>
2023-08-07 22:08:24 -0400

    Se agrega la base de filtro de precios

--- src/containers/Shop.jsx
+++ src/containers/Shop.jsx
@@ -10,6 +10,7 @@ import { connect } from 'react-redux'
 import { get_categories } from '../redux/actions/categories'
 import { get_products, get_filtered_products } from '../redux/actions/products'
 import ProductCard from '../components/product/ProductCard'
+import { PlusSmallIcon } from '@heroicons/react/24/outline'
 
 
 const sortOptions = [
@@ -253,48 +254,26 @@ const Shop = ({
                                 }
                             </ul>
 
-                            {filters.map((section) => (
-                                <Disclosure as="div" key={section.id} className="border-t border-gray-200 px-4 py-6">
-                                {({ open }) => (
-                                    <>
-                                    <h3 className="-mx-2 -my-3 flow-root">
-                                        <Disclosure.Button className="px-2 py-3 bg-white w-full flex items-center justify-between text-gray-400 hover:text-gray-500">
-                                        <span className="font-medium text-gray-900">{section.name}</span>
-                                        <span className="ml-6 flex items-center">
-                                            {open ? (
-                                            <MinusSmallIcon className="h-5 w-5" aria-hidden="true" />
-                                            ) : (
-                                            <PlusIcon className="h-5 w-5" aria-hidden="true" />
-                                            )}
-                                        </span>
-                                        </Disclosure.Button>
-                                    </h3>
-                                    <Disclosure.Panel className="pt-6">
-                                        <div className="space-y-6">
-                                        {section.options.map((option, optionIdx) => (
-                                            <div key={option.value} className="flex items-center">
-                                            <input
-                                                id={`filter-mobile-${section.id}-${optionIdx}`}
-                                                name={`${section.id}[]`}
-                                                defaultValue={option.value}
-                                                type="checkbox"
-                                                defaultChecked={option.checked}
-                                                className="h-4 w-4 border-gray-300 rounded text-indigo-600 focus:ring-indigo-500"
-                                            />
-                                            <label
-                                                htmlFor={`filter-mobile-${section.id}-${optionIdx}`}
-                                                className="ml-3 min-w-0 flex-1 text-gray-500"
-                                            >
-                                                {option.label}
-                                            </label>
-                                            </div>
-                                        ))}
-                                        </div>
-                                    </Disclosure.Panel>
-                                    </>
-                                )}
+                            
+                                <Disclosure as="div" className="border-t border-gray-200 px-4 py-6">
+                                    {({ open }) => (
+                                        <>
+                                        <h3 className='-mx-2 -my-3 flow-root'>
+                                            <Disclosure.Button className="px-2 py-3 bg-white w-full flex items-center justify-between text-gray-400 hover:text-gray-500">
+                                                <span className='font-sofiapro-regular text-gray-900'>Prices</span>
+                                                <span className='ml-6 flex items-center'>
+                                                    {open ? (
+                                                        <MinusSmallIcon className='h-5 w-5' aria-hidden='true' />
+                                                    ) : (
+                                                        <PlusSmallIcon className='h-5 w-5' aria-hidden='true' />
+                                                    )}
+                                                </span>
+                                            </Disclosure.Button>
+                                        </h3>
+                                        </>
+                                    )}
                                 </Disclosure>
-                            ))}
+                           
                             </form>
                         </div>
                         </Transition.Child>
