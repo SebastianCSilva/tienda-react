0fe4a18f2efb71801bf26779dd60015ee5bf1360
SebastianCSilva <sebastian.cornejo.silva@gmail.com>
2023-10-26 23:23:26 -0300

    se agrega funciones para mostrar items, se recupera la informacion de carrito del estado

--- src/containers/pages/Cart.jsx
+++ src/containers/pages/Cart.jsx
@@ -11,24 +11,42 @@ import { useEffect } from "react";
 const Cart = ({
     get_items,
     get_total,
-    get_item_total
+    get_item_total,
+    isAuthenticated,
+    items,
+    amount,
+    compare_amount,
+    total_items
 }) => {
 
     useEffect(() => {
+        window.scrollTo(0, 0);
         get_items()
         get_total()
         get_item_total()
     }, [])
 
+    const showItems = () => {
+        return(
+            <div>
+                <h4>Your cart has {total_items} item(s)</h4>
+            </div>
+        )
+    }
+
     return(
         <Layout>
-            cart
+            {showItems()}
         </Layout>
     )
 }
 
 const mapStateToProps = state => ({
-
+    isAuthenticated: state.Auth.isAuthenticated,
+    items: state.Cart.items,
+    amount: state.Cart.amount,
+    compare_amount: state.Cart.compare_amount,
+    total_items: state.Cart.total_items
 })
 
 export default connect(mapStateToProps,{
