a9b411ed197a5bb5c5c992ab7d70e1148f345416
SebastianCSilva <sebastian.cornejo.silva@gmail.com>
2023-10-28 00:21:11 -0300

    Se agrega diseño de los items dentro del carrito, junto con funciones para form de data y cantidades de items

--- src/components/cart/CartItem.js
+++ src/components/cart/CartItem.js
@@ -1,8 +1,98 @@
-const CartItem = () =>{
+import { useEffect, useState } from "react"
+
+const CartItem = ({
+    item,
+    count,
+    update_item,
+    remove_item,
+    render,
+    setRender
+}) =>{
+
+    const [formData, setFormData] = useState({
+        item_count: 1
+    });
+
+    const { item_count } = formData;
+
+    useEffect(() => {
+        if(count)
+            setFormData({ ...formData, item_count: count});
+    }, [count]);
+
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    
+
     return(
-        <div>
-            cart item
-        </div>
+        <li className="flex py-6 sm:py-10">
+            <div className="flex-shrink-0">
+            <img
+                src={item.product.photo}
+                alt=""
+                className="w-24 h-24 rounded-md object-center object-cover sm:w-48 sm:h-48"
+            />
+            </div>
+
+            <div className="ml-4 flex-1 flex flex-col justify-between sm:ml-6">
+                <div className="relative pr-9 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:pr-0">
+                    <div>
+                    <div className="flex justify-between">
+                        <h3 className="text-sm">
+                        <Link to={`/product/${item.product.id}`} className="font-medium text-gray-700 hover:text-gray-800">
+                            {item.product.name}
+                        </Link>
+                        </h3>
+                    </div>
+                    <div className="mt-1 flex text-sm">
+                        <p className="text-gray-500">Color</p>
+                        {/*product.size ? (
+                        <p className="ml-4 pl-4 border-l border-gray-200 text-gray-500">{product.size}</p>
+                        ) : null */}
+                    </div>
+                    <p className="mt-1 text-sm font-medium text-gray-900">$ {item.product.price}</p>
+                    </div>
+
+                    <div className="mt-4 sm:mt-0 sm:pr-9">
+                    <label htmlFor='item_count' className="sr-only">
+                        Quantity, {item.product.name}
+                    </label>
+                    <select
+                        name='item_count'
+                        onChange={e => onChange(e)}
+                        value={item_count}
+                        className="max-w-full rounded-md border border-gray-300 py-1.5 text-base leading-5 font-medium text-gray-700 text-left shadow-sm focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    >
+                        <option value={1}>1</option>
+                        <option value={2}>2</option>
+                        <option value={3}>3</option>
+                        <option value={4}>4</option>
+                        <option value={5}>5</option>
+                        <option value={6}>6</option>
+                        <option value={7}>7</option>
+                        <option value={8}>8</option>
+                    </select>
+
+                    <div className="absolute top-0 right-0">
+                        <button type="button" className="-m-2 p-2 inline-flex text-gray-400 hover:text-gray-500">
+                        <span className="sr-only">Remove</span>
+                        <XIcon className="h-5 w-5" aria-hidden="true" />
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <p className="mt-4 flex text-sm text-gray-700 space-x-2">
+                {product.inStock ? (
+                <CheckIcon className="flex-shrink-0 h-5 w-5 text-green-500" aria-hidden="true" />
+                ) : (
+                <ClockIcon className="flex-shrink-0 h-5 w-5 text-gray-300" aria-hidden="true" />
+                )}
+
+                <span>{product.inStock ? 'In stock' : `Ships in ${product.leadTime}`}</span>
+            </p>
+            </div>
+        </li>
     )
 }
 export default CartItem
