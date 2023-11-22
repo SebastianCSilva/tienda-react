5956a42ef5fbef08e8a0928710de13ad4dfafec8
SebastianCSilva <sebastian.cornejo.silva@gmail.com>
2023-11-21 23:36:40 -0300

    Se agrega los estados de las ordenes y listado de paises

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from apps.product.models import Product
+from .countries import Countries
+from datetime import datetime
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
-# Create your models here.
+
+class Order(models.Model):
+    class OrderStatus(models.TextChoices):
+        not_processed = 'not_processed'
+        processed = 'processed'
+        shipping = 'shipping'
+        delivered = 'delivered'
+        cancelled = 'cancelled'
+
