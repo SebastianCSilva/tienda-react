e197f5183fb1600aacfee7eb0b0ca56aa4a6a3ab
SebastianCSilva <sebastian.cornejo.silva@gmail.com>
2023-11-28 23:42:02 -0300

    Se crea la vista para el listado de ordenes de un usuario

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Order, OrderItem
 
-# Create your views here.
+class ListOrdersView(APIView):
+    def get(self, request, format=None):
+        user = self.request.user
+        try:
+            orders = Order.objects.order_by('-date_issued').filter(user=user)
+            result = []
+
+            for order in orders:
+                item = {}
+                item['status'] = order.status
+                item['transaction_id'] = order.transaction_id
+                item['amount'] = order.amount
+                item['shipping_price'] = order.shipping_price
+                item['date_issued'] = order.date_issued
+
+                result.append(item)
+
+            return Response(
+                {'orders': result},
+                status=status.HTTP_200_OK
+            )
+
+        except:
+            return Response(
+                {'error': 'Something went wrong when retrieving orders'},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
