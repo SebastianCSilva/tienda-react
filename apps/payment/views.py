32751e0cba7c605bdcae40f8eb15b03935dbf183
SebastianCSilva <sebastian.cornejo.silva@gmail.com>
2023-12-04 23:56:47 -0300

    Se agrega las urls de payment, configuraciones para braintree en core ademas de las importaciones para trabajar la vista de payment

--- apps/payment/views.py
+++ apps/payment/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.conf import settings
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from apps.cart.models import Cart, CartItem
+from apps.orders.models import Order, OrderItem
+from apps.product.models import Product
+from apps.shipping.models import Shipping
+
+from django.core.mail import send_mail
+import braintree
+
