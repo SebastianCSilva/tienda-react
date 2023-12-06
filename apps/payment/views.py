21f005e5c66253a4e10af9a037987089ee29ef8c
SebastianCSilva <sebastian.cornejo.silva@gmail.com>
2023-12-05 23:11:40 -0300

    Se agrega la generaciond e token de braintree

--- apps/payment/views.py
+++ apps/payment/views.py
@@ -12,3 +12,26 @@ from apps.shipping.models import Shipping
 from django.core.mail import send_mail
 import braintree
 
+gateway = braintree.BraintreeGateway(
+    braintree.Configuration(
+        environment=settings.BT_ENVIRONMENT,
+        merchant_id=settings.BT_MERCHANT_ID,
+        public_key=settings.BT_PUBLIC_KEY,
+        private_key=settings.BT_PRIVATE_KEY
+    )
+)
+
+class GenerateTokenView(APIView):
+    def get(self, request, format=None):
+        try :
+            token = gateway.client_token.generate()
+
+            return Response(
+                {'braintree_token': token},
+                status=status.HTTP_200_OK
+            )
+        except:
+            return Response(
+                {'error': 'Something went wrong when retrieving braintree token'},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
