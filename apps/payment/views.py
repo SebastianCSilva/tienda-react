dde458251905e7a127a81f9942193afd725eb8c8
SebastianCSilva <sebastian.cornejo.silva@gmail.com>
2023-12-07 23:51:53 -0300

    Se agrega la vista para el procesado de pagos

--- apps/payment/views.py
+++ apps/payment/views.py
@@ -117,3 +117,192 @@ class GetPaymentTotalView(APIView):
                 {'error': 'Something went wrong when retrieving payment total information'},
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
+
+class ProcessPaymentView(APIView):
+    def post(self, request, format=None):
+        user = self.request.user
+        data = self.request.data
+
+        tax = 0.19
+
+        nonce = data['nonce']
+        shipping_id = str(data['shipping_id'])
+        # Cupon name
+
+        full_name = data['full_name']
+        address_line_1 = data['address_line_1']
+        address_line_2 = data['address_line_2']
+        city = data['city']
+        state_province_region = data['state_province_region']
+        postal_zip_code = data['postal_zip_code']
+        country_region = data['country_region']
+        telephone_number = data['telephone_number']
+
+        # Revisar si datos de shipping son validos
+        if not Shipping.objects.filter(id__iexact=shipping_id).exists():
+            return Response(
+                {'error': 'Invalid shipping option'},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        
+        cart = Cart.objects.get(user=user)
+
+        #revisar si usuario tiene items en carrito
+        if not CartItem.objects.filter(cart=cart).exists():
+            return Response(
+                {'error': 'Need to have items in cart'},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        
+        cart_items = CartItem.objects.filter(cart=cart)
+
+        # Revisar si hay stock
+
+        for cart_item in cart_items:
+            if not Product.objects.filter(id=cart_item.product.id).exists():
+                return Response(
+                    {'error': 'Transaction failed, a product Id does not exist'},
+                    status=status.HTTP_404_NOT_FOUND
+                )
+            if int(cart_item.count) > int(cart_item.product.quantity):
+                return Response(
+                    {'error': 'Not enough items in stock'},
+                    status=status.HTTP_200_OK
+                )
+            
+        total_amount = 0.0
+
+        for cart_item in cart_items:
+            total_amount += (float(cart_item.product.price)
+                             * float(cart_item.count))
+            
+        # Cupones
+        #
+        #
+
+        total_amount += (total_amount * tax)
+
+        shipping = Shipping.objects.get(id=int(shipping_id))
+
+        shipping_name = shipping.name
+        shipping_time = shipping.time_to_delivery
+        shipping_price = shipping.price
+
+        total_amount += float(shipping_price)
+        total_amount = round(total_amount, 2)
+
+        try:
+            # Crear transaccion con braintree
+
+            newTransaction = gateway.transaction.sale(
+                {
+                    'amount': str(total_amount),
+                    'payment_method_nonce': str(nonce['nonce']),
+                    'options': {
+                        'submit_for_settlement': True
+                    }
+                }
+            )
+        except:
+            return Response(
+                {'error': 'Error processing the transaction'},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+        
+        if newTransaction.is_success or newTransaction.transaction:
+            for cart_item in cart_items:
+                update_producto = Product.objects.get(id=cart_item.product.id)
+
+                # Encontrar cantidad despues de compra
+                quantity = int(update_producto.quantity) - int(cart_item.count)
+
+                # Obtener cantidad de producto por vender
+                sold = int(update_producto.sold) + int(cart_item.count)
+
+                # Actualizar el producto
+                Product.objects.filter(id=cart_item.product.id).update(
+                    quantity=quantity, sold=sold
+                )
+        
+            try:
+                order = Order.objects.create(
+                    user=user,
+                    transaction_id=newTransaction.transaction.id,
+                    amount=total_amount,
+                    full_name = full_name,
+                    address_line_1 = address_line_1,
+                    address_line_2 = address_line_2,
+                    city = city,
+                    state_province_region = state_province_region,
+                    postal_zip_code = postal_zip_code,
+                    country_region = country_region,
+                    telephone_number = telephone_number,
+                    shipping_name = shipping_name,
+                    shipping_time = shipping_time,
+                    shipping_price = float(shipping_price)
+                )
+            except:
+                return Response(
+                    {'error': 'Transaction succeeded but failed to create the order'},
+                    status=status.HTTP_500_INTERNAL_SERVER_ERROR
+                )
+            
+            for cart_item in cart_items:
+                try:
+                    # Obtener el producto de la instancia
+                    product = Product.objects.get(id=cart_item.product.id)
+
+                    OrderItem.objects.create(
+                        product=product,
+                        order=order,
+                        name=product.name,
+                        price=cart_item.product.price,
+                        count=cart_item.count
+                    )
+                except:
+                    return Response(
+                        {'error': 'Transaction succeeded and order created, but failed to create an order item'},
+                        status=status.HTTP_500_INTERNAL_SERVER_ERROR
+                    )
+                
+            try:
+                send_mail(
+                    'Your Order Details',
+                    'Hey ' + full_name + ','
+                    + '\n\nWe recieved your order!'
+                    + '\n\nGive us some time to process your order and ship it out to you.'
+                    + '\n\nYou can go on your user dashboard to check the status of your order.'
+                    + '\n\nSincerely,'
+                    + '\nShop Time',
+                    'mail@xohayo.com',
+                    [user.email],
+                    fail_silently=False
+                )
+            except:
+                return Response(
+                    {'error': 'Transaction succeeded and order created, but failed to send email'},
+                    status=status.HTTP_500_INTERNAL_SERVER_ERROR
+                )
+            
+            try:
+                # Vaciar carrito de compras
+                CartItem.objects.filter(cart=cart).delete()
+
+                # Actualizar carrito
+                Cart.objects.filter(user=user).update(total_items=0)
+            except:
+                return Response(
+                    {'error': 'Transaction succeeded and order successful, but failed to clear cart'},
+                    status=status.HTTP_500_INTERNAL_SERVER_ERROR
+                )
+            
+            return Response(
+                {'success': 'Transaction successful and order was created'},
+                status=status.HTTP_200_OK
+            )
+    
+        else:
+            return Response(
+                {'error': 'Transaction failed'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
