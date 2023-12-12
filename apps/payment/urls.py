66af510a95bad40cc32168a0047b4babac4d2e84
SebastianCSilva <sebastian.cornejo.silva@gmail.com>
2023-12-11 22:55:56 -0300

    se agregan los endpoint del payment

--- apps/payment/urls.py
+++ apps/payment/urls.py
@@ -4,4 +4,7 @@ from .views import *
 app_name = "payment"
 
 urlpatterns = [
+    path('get-payment-total', GetPaymentTotalView.as_view()),
+    path('get-token', GenerateTokenView.as_view()),
+    path('make-payment', ProcessPaymentView.as_view())
 ]
