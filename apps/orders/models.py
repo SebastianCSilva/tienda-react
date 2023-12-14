b047d6b121b768b68f360c06022ebce4c715e670
SebastianCSilva <sebastian.cornejo.silva@gmail.com>
2023-12-13 23:35:27 -0300

    Se arregla problema de tipeo en modelo de orderitem

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -42,7 +42,7 @@ class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
-    price = models.DecimalField(max_length=5, decimal_places=2)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
     count = models.IntegerField()
     date_added = models.DateTimeField(default=datetime.now)
 
