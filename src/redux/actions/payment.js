e8fbec759c6aa4f429312e49551d73ffe44a6b92
SebastianCSilva <sebastian.cornejo.silva@gmail.com>
2023-12-27 22:54:52 -0300

    Se agrega acciones de payment para obtener el total

---
+++ src/redux/actions/payment.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { setAlert } from  './alert';
+import { get_item_total } from './cart';
+import {
+    GET_PAYMENT_TOTAL_SUCCESS,
+    GET_PAYMENT_TOTAL_FAIL,
+    LOAD_BT_TOKEN_SUCCESS,
+    LOAD_BT_TOKEN_FAIL,
+    PAYMENT_SUCCESS,
+    PAYMENT_FAIL,
+    RESET_PAYMENT_INFO,
+    SET_PAYMENT_LOADING,
+    REMOVE_PAYMENT_LOADING
+} from './types';
+
+export const get_payment_total = (shipping_id) => async dispatch => {
+    const config = {
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': `JWT ${localStorage.getItem('access')}`
+        }
+
+    };
+
+    try {
+        const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/payment/get-payment-total?shipping_id=${shipping_id}`, config)
+        
+        if (res.status === 200 && !res.data.error){
+            dispatch({
+                type: GET_PAYMENT_TOTAL_SUCCESS,
+                payload: res.data
+            });
+        } else {
+            dispatch({
+                type: GET_PAYMENT_TOTAL_FAIL
+            });
+        }
+    } catch(err){
+        dispatch({
+            type: GET_PAYMENT_TOTAL_FAIL
+        });
+    }
+}
